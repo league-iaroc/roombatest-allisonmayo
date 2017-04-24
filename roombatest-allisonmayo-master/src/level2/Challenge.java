07e46c488f56d6f3d368bdb27b71f60c6d3b0f17
allisonmayo <allison.mayo@jointheleague.org>
2017-04-24 14:53:57 -0700

    Add files via upload

---
+++ roombatest-allisonmayo-master/src/level2/Challenge.java
@@ -0,0 +1,25 @@
+package level2;
+
+import level1.Sonar;
+import processing.core.PApplet;
+
+public class Challenge extends Head {
+	Roomba roomba;
+
+	public static void main(String[] args) {
+		PApplet.main("level2.Processing");
+	}
+
+	public Challenge(Roomba roomba) {
+		super(roomba);
+		Sonar sonar = new Sonar(roomba);
+	}
+
+	public void initialize() {
+		driveDrive(500,500);
+		sleep(1000);	}
+
+	public void loop() {
+	
+	}
+}
